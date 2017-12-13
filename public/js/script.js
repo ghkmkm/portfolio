a7f95dea72ca94de3205c5ea6e6bff09871ab32e
DESKTOP-E7CEOVI\05 <webkmkm@gmail.com>
2017-12-13 11:42:04 +0900

    あとこれだけ：jsの調整

--- public/js/script.js
+++ public/js/script.js
@@ -5,7 +5,7 @@ $(function () {
         $(this).toggleClass("active");
         $("#gnav").stop().slideToggle();
         return false;
-    })
+    });
 
 
     $(".btn-top-320").click(function () {
@@ -51,36 +51,11 @@ $(function () {
         $('.main-inner').fullpage({
             menu: "#gnav",
             anchors: ["page1", "page2", "page3", "page4", "page5"],
+            scrollOverflow: true,
+            scrollOverflowReset: true,
+            
             responsiveWidth: 768
         });
     });
-
-
-
-    //    var $win = $(window);
-    //
-    //    $win.on('load resize', function () {
-    //        var windowWidth = window.innerWidth;
-    //
-    //        if (windowWidth > 768) {
-    //            $(document).ready(function () {
-    //                $('.main-inner').fullpage({
-    //                    menu: "#gnav",
-    //                    anchors: ["page1", "page2", "page3", "page4", "page5"]
-    //                });
-    //            });
-    //        } else {
-    //            
-    //            // スムーススクロール---------------------
-    //
-    //            $("#gnav li a").click(function () {
-    //                var target = $($(this).attr("href")).offset().top;
-    //                $("html,body").stop().animate({
-    //                    scrollTop: target
-    //                }, 400);
-    //                return false;
-    //            });
-    //        }
-    //    });
 
 });

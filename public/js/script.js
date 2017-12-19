e8bc257ed8182a27bc1c326c8993f8152e71d449
DESKTOP-E7CEOVI\05 <webkmkm@gmail.com>
2017-12-19 14:36:11 +0900

    2017/12/19 現在

--- public/js/script.js
+++ public/js/script.js
@@ -8,9 +8,9 @@ $(function () {
     });
 
 
-    // Modal win.---------------------------------
+    // Modal win.たて用-----------------------------
 
-    $("#gallery a").click(function () {
+    $(".ga-y").click(function () {
 
         $("body").append("<div id='bg'>");
         $("body").append("<div id='photo'>");
@@ -32,6 +32,66 @@ $(function () {
             });
 
             $("#photo").fadeOut(function () {
+                $(this).remove();
+            });
+        });
+        return false;
+    });
+    
+    // Modal win.よこ用-----------------------------
+
+    $(".ga-x").click(function () {
+
+        $("body").append("<div id='bg'>");
+        $("body").append("<div id='photo2'>");
+
+        $("#bg").hide();
+        $("#photo2").hide();
+
+        $("#photo2").html("<img>");
+
+        $("#photo2 img").attr("src", $(this).attr("href"));
+
+        $("#bg").fadeIn();
+        $("#photo2").fadeIn();
+
+        $("#bg").click(function () {
+
+            $(this).fadeOut(function () {
+                $(this).remove();
+            });
+
+            $("#photo2").fadeOut(function () {
+                $(this).remove();
+            });
+        });
+        return false;
+    });
+    
+    // Modal win.中間用-----------------------------
+
+    $(".ga-xy").click(function () {
+
+        $("body").append("<div id='bg'>");
+        $("body").append("<div id='photo3'>");
+
+        $("#bg").hide();
+        $("#photo3").hide();
+
+        $("#photo3").html("<img>");
+
+        $("#photo3 img").attr("src", $(this).attr("href"));
+
+        $("#bg").fadeIn();
+        $("#photo3").fadeIn();
+
+        $("#bg").click(function () {
+
+            $(this).fadeOut(function () {
+                $(this).remove();
+            });
+
+            $("#photo3").fadeOut(function () {
                 $(this).remove();
             });
         });
